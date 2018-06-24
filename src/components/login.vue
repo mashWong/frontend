41cc01e7d95d87e64acc9c800087e960dce3b690
mashWong <1115310504@qq.com>
2018-06-24 17:56:58 +0800

    搭建基础框架

---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <input placeholder="账号">
+    <input placeholder="密码">
+    <button type="submit">提交</button>
+  </form>
+</template>
+<script>
+  export default{
+    name: 'login',
+
+  }
+</script>
